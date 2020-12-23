3a1e83daeba294d1d41e3798b2c0236312e5bda2
陶思淇 <1870894297@qq.com>
2020-12-23 23:29:21 +0800

    整理

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import mitt from './assets/js/mitt'
+import mitt from './assets/mitt'
 const emitter =new mitt();
 const app = createApp(App);
 app.config.globalProperties.$emitter = emitter;
